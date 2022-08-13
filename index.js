7e64ab6b9b4867e6ae01186960a9f46ca76046dd
Edison Loor <edison.david.loor@gmail.com>
2022-08-13 13:27:49 -0500

    feature flag added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const nodecache = require('node-cache');
+const Flagsmith = require("flagsmith-nodejs");
 
 const app = express();
 const port = process.env.PORT || '3000';
@@ -8,14 +10,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+const flagsmith = new Flagsmith({
+    environmentKey: '7wGJnGjuMzoXZBLp6Hi3bs',
+    cache: new nodecache({stdTTL : 10, checkperiod: 10}),
+});
+
+
+app.get('/', async (req, res) => {
+    let showFooterIcons = false;
+    try {
+        let footerFeatures = await flagsmith.getEnvironmentFlags();
+        showFooterIcons =footerFeatures.isFeatureEnabled('show_footer_icons');
+        console.log(showFooterIcons);
+    } catch (e) {
+        console.log(`Error connecting to flagsmith - ${e.getMessage} `, e);
+    }
+
+    console.log(`show footer icons: ${showFooterIcons}`);
     res.render(
         'index',
         {
             title: 'Coming Soon!',
             mainText: 'Eventually Podcast',
             subText: `Drop your email address below and we will let you know when we launch the Eventually podcast. 
-      <br>Brought to you by amazing people`
+      <br>Brought to you by amazing people`,
+            showFooterIcons,
         }
     );
 });
